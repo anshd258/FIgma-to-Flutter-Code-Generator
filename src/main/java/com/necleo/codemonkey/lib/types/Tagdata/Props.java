70f1af1691b57b7c66c81a66b6b7e2402a6f3568
anshd258 <anshd258@gmail.com>
2023-04-17 23:31:58 +0530

    added tag data node updadtes

---
+++ src/main/java/com/necleo/codemonkey/lib/types/Tagdata/Props.java
@@ -0,0 +1,21 @@
+package com.necleo.codemonkey.lib.types.Tagdata;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@SuperBuilder
+@Jacksonized
+public class Props {
+    String page;
+    String linkType;
+    String pageUrl;
+}
---
+++ src/main/java/com/necleo/codemonkey/lib/types/Tagdata/Tag.java
@@ -0,0 +1,20 @@
+package com.necleo.codemonkey.lib.types.Tagdata;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@SuperBuilder
+@Jacksonized
+public class Tag {
+    Props props;
+
+}
